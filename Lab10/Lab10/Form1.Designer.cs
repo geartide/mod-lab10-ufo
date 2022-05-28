27f57eaadcf4ef1323399433eb7713150bf26042
geartide <80594346+geartide@users.noreply.github.com>
2022-05-28 21:45:46 +0300

    Выполнить работу

---
+++ Lab10/Lab10/Form1.Designer.cs
@@ -0,0 +1,240 @@
+﻿
+namespace Lab10
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tbX1 = new System.Windows.Forms.TextBox();
+            this.tbY1 = new System.Windows.Forms.TextBox();
+            this.tbX2 = new System.Windows.Forms.TextBox();
+            this.tbY2 = new System.Windows.Forms.TextBox();
+            this.PleaseStart = new System.Windows.Forms.Button();
+            this.MethodComboBox = new System.Windows.Forms.ComboBox();
+            this.tbPrecision = new System.Windows.Forms.TextBox();
+            this.tbReqTerms = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // tbX1
+            // 
+            this.tbX1.Location = new System.Drawing.Point(22, 557);
+            this.tbX1.Margin = new System.Windows.Forms.Padding(2);
+            this.tbX1.Name = "tbX1";
+            this.tbX1.Size = new System.Drawing.Size(68, 20);
+            this.tbX1.TabIndex = 0;
+            this.tbX1.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.tbXY_CheckChar);
+            // 
+            // tbY1
+            // 
+            this.tbY1.Location = new System.Drawing.Point(22, 581);
+            this.tbY1.Margin = new System.Windows.Forms.Padding(2);
+            this.tbY1.Name = "tbY1";
+            this.tbY1.Size = new System.Drawing.Size(68, 20);
+            this.tbY1.TabIndex = 1;
+            this.tbY1.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.tbXY_CheckChar);
+            // 
+            // tbX2
+            // 
+            this.tbX2.Location = new System.Drawing.Point(94, 557);
+            this.tbX2.Margin = new System.Windows.Forms.Padding(2);
+            this.tbX2.Name = "tbX2";
+            this.tbX2.Size = new System.Drawing.Size(68, 20);
+            this.tbX2.TabIndex = 2;
+            this.tbX2.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.tbXY_CheckChar);
+            // 
+            // tbY2
+            // 
+            this.tbY2.Location = new System.Drawing.Point(94, 581);
+            this.tbY2.Margin = new System.Windows.Forms.Padding(2);
+            this.tbY2.Name = "tbY2";
+            this.tbY2.Size = new System.Drawing.Size(68, 20);
+            this.tbY2.TabIndex = 3;
+            this.tbY2.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.tbXY_CheckChar);
+            // 
+            // PleaseStart
+            // 
+            this.PleaseStart.Font = new System.Drawing.Font("Microsoft Sans Serif", 12.22642F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.PleaseStart.Location = new System.Drawing.Point(676, 557);
+            this.PleaseStart.Margin = new System.Windows.Forms.Padding(2);
+            this.PleaseStart.Name = "PleaseStart";
+            this.PleaseStart.Size = new System.Drawing.Size(276, 45);
+            this.PleaseStart.TabIndex = 4;
+            this.PleaseStart.Text = "Крути катушку!";
+            this.PleaseStart.UseVisualStyleBackColor = true;
+            this.PleaseStart.Click += new System.EventHandler(this.PleaseStart_Click);
+            // 
+            // MethodComboBox
+            // 
+            this.MethodComboBox.FormattingEnabled = true;
+            this.MethodComboBox.Items.AddRange(new object[] {
+            "Уравнение прямой",
+            "Использование угла"});
+            this.MethodComboBox.Location = new System.Drawing.Point(166, 581);
+            this.MethodComboBox.Margin = new System.Windows.Forms.Padding(2);
+            this.MethodComboBox.Name = "MethodComboBox";
+            this.MethodComboBox.Size = new System.Drawing.Size(140, 21);
+            this.MethodComboBox.TabIndex = 5;
+            this.MethodComboBox.SelectedIndexChanged += new System.EventHandler(this.MethodComboBox_SelectedIndexChanged);
+            // 
+            // tbPrecision
+            // 
+            this.tbPrecision.Location = new System.Drawing.Point(407, 581);
+            this.tbPrecision.Margin = new System.Windows.Forms.Padding(2);
+            this.tbPrecision.Name = "tbPrecision";
+            this.tbPrecision.Size = new System.Drawing.Size(44, 20);
+            this.tbPrecision.TabIndex = 6;
+            this.tbPrecision.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.tbPrecision_CheckChar);
+            // 
+            // tbReqTerms
+            // 
+            this.tbReqTerms.Location = new System.Drawing.Point(604, 582);
+            this.tbReqTerms.Margin = new System.Windows.Forms.Padding(2);
+            this.tbReqTerms.Name = "tbReqTerms";
+            this.tbReqTerms.Size = new System.Drawing.Size(68, 20);
+            this.tbReqTerms.TabIndex = 7;
+            this.tbReqTerms.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.tbXY_CheckChar);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(19, 540);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(50, 15);
+            this.label1.TabIndex = 8;
+            this.label1.Text = "Точка 1";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(91, 540);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(50, 15);
+            this.label2.TabIndex = 9;
+            this.label2.Text = "Точка 2";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(2, 560);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(15, 15);
+            this.label3.TabIndex = 10;
+            this.label3.Text = "X";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(3, 584);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(14, 15);
+            this.label4.TabIndex = 11;
+            this.label4.Text = "Y";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(167, 560);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(95, 15);
+            this.label5.TabIndex = 12;
+            this.label5.Text = "Выбор метода:";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(311, 584);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(91, 15);
+            this.label6.TabIndex = 13;
+            this.label6.Text = "Точность (пкс)";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(456, 584);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(143, 15);
+            this.label7.TabIndex = 14;
+            this.label7.Text = "Членов в ряде Тейлора";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(963, 612);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.tbReqTerms);
+            this.Controls.Add(this.tbPrecision);
+            this.Controls.Add(this.MethodComboBox);
+            this.Controls.Add(this.PleaseStart);
+            this.Controls.Add(this.tbY2);
+            this.Controls.Add(this.tbX2);
+            this.Controls.Add(this.tbY1);
+            this.Controls.Add(this.tbX1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.Margin = new System.Windows.Forms.Padding(2);
+            this.Name = "Form1";
+            this.Text = "Рисование прямых";
+            this.Paint += new System.Windows.Forms.PaintEventHandler(this.Form1_Paint);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox tbX1;
+        private System.Windows.Forms.TextBox tbY1;
+        private System.Windows.Forms.TextBox tbX2;
+        private System.Windows.Forms.TextBox tbY2;
+        private System.Windows.Forms.Button PleaseStart;
+        private System.Windows.Forms.ComboBox MethodComboBox;
+        private System.Windows.Forms.TextBox tbPrecision;
+        private System.Windows.Forms.TextBox tbReqTerms;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+    }
+}
+
---
+++ Lab10/Lab10/Form1.cs
@@ -0,0 +1,229 @@
+﻿using System;
+using System.Drawing;
+using System.Threading;
+using System.Windows.Forms;
+
+namespace Lab10
+{
+    public partial class Form1 : Form
+    {
+
+        public Form1()
+        {
+            InitializeComponent();
+            tbPrecision.Enabled = false;
+            tbReqTerms.Enabled = false;
+        }
+
+        private void PleaseStart_Click(object sender, EventArgs e)
+        {
+            int temp = 0;
+            float tempF = 0;
+            uint tempU = 0;
+            if (!Int32.TryParse(tbX1.Text, out temp)
+             || !Int32.TryParse(tbY1.Text, out temp)
+             || !Int32.TryParse(tbX2.Text, out temp)
+             || !Int32.TryParse(tbY2.Text, out temp)
+             || (MethodComboBox.SelectedIndex == 1
+                    && (!float.TryParse(tbPrecision.Text, out tempF) || !uint.TryParse(tbReqTerms.Text, out tempU) || tempU == 0)))
+            {
+                MessageBox.Show("Неправильно введены данные");
+                return;
+            }
+
+            Point Start = new Point(int.Parse(tbX1.Text), int.Parse(tbY1.Text));
+            Point End = new Point(int.Parse(tbX2.Text), int.Parse(tbY2.Text));
+
+            switch (MethodComboBox.SelectedIndex)
+            {
+                case 0:
+                    UseLineEquation(Start, End);
+                    break;
+
+                case 1:
+                    int TermCount = int.Parse(tbReqTerms.Text);
+                    float Precision = float.Parse(tbPrecision.Text);
+                    UseDifferentialApproach(Start, End, Precision, TermCount);
+                    break;
+
+                default:
+                    break;
+            }
+        }
+
+        private void tbXY_CheckChar(object sender, KeyPressEventArgs e)
+        {
+            char ch = e.KeyChar;
+            if (!(Char.IsDigit(ch) || ch == (char)Keys.Back || ch == '-'))
+            {
+                e.Handled = true;
+            }
+        }
+
+        private void tbPrecision_CheckChar(object sender, KeyPressEventArgs e)
+        {
+            char ch = e.KeyChar;
+            if (!(Char.IsDigit(ch) || ch == (char)Keys.Back || ch == ',' || ch == '.'))
+            {
+                e.Handled = true;
+            }
+        }
+
+        private void UseLineEquation(Point Start, Point End)
+        {
+            if(End.X - Start.X < 0.01)
+            {
+                MessageBox.Show("Прямая не может быть вертикалью");
+                return;
+            }
+
+            double k = ((double)(End.Y - Start.Y)) / (End.X - Start.X);
+            double h = Start.Y - k * Start.X;
+
+            Graphics graphics = GetScaledGraphics();
+            graphics.DrawEllipse(new Pen(Color.Blue, 5), End.X - 5, End.Y - 5, 10, 10);
+            graphics.DrawEllipse(new Pen(Color.Green, 5), Start.X - 5, Start.Y - 5, 10, 10);
+
+            int step = (End.X > Start.X) ? 1 : -1;
+
+            int X = Start.X;
+            while (X != End.X)
+            {
+                Thread.Sleep(16);
+                graphics.DrawEllipse(new Pen(Color.Red, 5), new RectangleF(X, (float)(k * X + h), 1, 1));
+                X += step;
+            }
+        }
+
+        public void UseDifferentialApproach(Point Start, Point End, float Precision, int RequiredTerms)
+        {
+            double x = Start.X;
+            double y = Start.Y;
+            int step_mult = 1;
+
+            Pen black_pen = new Pen(Color.Black, 5);
+            Pen blue_pen = new Pen(Color.Black, 5);
+            Pen green_pen = new Pen(Color.Green, 5);
+            Pen red_pen = new Pen(Color.Red, 5);
+
+            // not slope!
+            double angle = Atan(Math.Abs(End.Y - Start.Y) / (double)Math.Abs(End.X - Start.X), RequiredTerms) * 180 / Math.PI;
+
+            double distance = Math.Sqrt(Math.Pow(End.Y - Start.Y, 2) + Math.Pow(End.X - Start.X, 2));
+
+            double sine = Sin(angle, RequiredTerms);
+            double cosine = Cos(angle, RequiredTerms);
+
+            Graphics graphics = GetScaledGraphics();
+
+            graphics.DrawEllipse(red_pen, End.X - Precision, End.Y - Precision, Precision * 2, Precision * 2);
+            graphics.DrawEllipse(black_pen, Start.X - 5, Start.Y - 5, 10, 10);
+
+            int Xstep = (End.X > Start.X) ? 1 : -1;
+            int Ystep = (End.Y > Start.Y) ? 1 : -1;
+
+            while (distance > Precision)
+            {
+                x += Xstep * (cosine * step_mult);
+                y += Ystep * (sine * step_mult);
+
+                graphics.DrawEllipse(black_pen, (int)x, (int)y, 2, 2);
+                double PrevDistance = distance;
+                distance = Math.Sqrt(Math.Pow(End.Y - y, 2) + Math.Pow(End.X - x, 2));
+
+                if (PrevDistance < distance)
+                {
+                    MessageBox.Show("Не попали в окрестность точки");
+                    return;
+                }
+            }
+            MessageBox.Show("Передвижение закончено в пределах заданной окрестности точки");
+        }
+
+        private Graphics GetScaledGraphics()
+        {
+            Graphics result = CreateGraphics();
+
+            result.TranslateTransform(Width / 2, Height / 2);
+            result.ScaleTransform(0.5f, 0.5f);
+
+            return result;
+        }
+
+        private void MethodComboBox_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            bool enabledness = false;
+
+            if (MethodComboBox.SelectedIndex == 1)
+                enabledness = true;
+
+            tbPrecision.Enabled = enabledness;
+            tbReqTerms.Enabled = enabledness;
+        }
+
+        private void Form1_Paint(object sender, PaintEventArgs e)
+        {
+            Pen blue_pen = new Pen(Color.Turquoise, 4);
+            Graphics graphics = GetScaledGraphics();
+
+            // draw axes
+            graphics.DrawLine(blue_pen, new Point(0, 10000), new Point(0, -10000));
+            graphics.DrawLine(blue_pen, new Point(-10000, 0), new Point(10000, 0));
+        }
+
+        int fact(int n)
+        {
+            int retval = 1;
+            if (n <= 1) return retval;
+            for (int i = 2; i <= n; i++)
+                retval *= i;
+            return retval;
+        }
+
+        private double Sin(double in_angle, int TermCount)
+        {
+            double retval = 0;
+            for (int i = 0; i < TermCount; i++)
+            {
+                int factorial = fact(2 * i + 1); // Передайте им, что я не хотел этого делать
+                retval += Math.Pow(-1, i) * (Math.Pow(in_angle * Math.PI / 180, 2 * i + 1) / factorial);
+            }
+            return retval;
+        }
+        private double Cos(double in_angle, int TermCount)
+        {
+            double retval = 0;
+            for (int i = 0; i < TermCount; i++)
+            {
+                int factorial = fact(2 * i);
+                retval += Math.Pow(-1, i) * (Math.Pow(in_angle * Math.PI / 180, 2 * i) / factorial);
+            }
+            return retval;
+        }
+
+        double Atan(double tangent, int TermCount)
+        {
+            double retval = 0;
+            if (-1 <= tangent && tangent <= 1)
+            {
+                for (int i = 0; i < TermCount; i++)
+                {
+                    retval += Math.Pow(-1, i) * Math.Pow(tangent, 2 * i + 1) / (2 * i + 1);
+                }
+            }
+            else
+            {
+                int sign;
+                if (tangent >= 1) sign = 1;
+                else sign = -1;
+
+                retval = sign * Math.PI / 2;
+                for (int i = 0; i < TermCount; i++)
+                {
+                    retval -= Math.Pow(-1, i) / ((2 * i + 1) * Math.Pow(tangent, 2 * i + 1));
+                }
+            }
+            return retval;
+        }
+    }
+}
